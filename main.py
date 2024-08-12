81ba74b0b350fa3838b982e36fd4aa085b7b7e2f
Hugo Messias <hugomessias9@gmail.com>
2024-08-12 13:14:20 -0300

    Implemented functionality to automatically get all download data from every episode within a given season.

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import json
 import re
 
+import requests
+from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.firefox.options import Options
@@ -13,6 +15,7 @@ def get_episodes_info(season: int, url: str, headless: bool = True):
         options.add_argument("--headless") # hide browser window
 
     # start browser inside a context manager
+    print("Starting browser...")
     with webdriver.Firefox(options=options) as browser:
         # wait for page to load
         browser.implicitly_wait(15)
@@ -21,6 +24,7 @@ def get_episodes_info(season: int, url: str, headless: bool = True):
         browser.get(url)
 
         # find the 'choose season' button and click it
+        print("Searching for season...")
         select_season_btn = browser.find_element(By.CLASS_NAME, "seasons")
         select_season_btn.click()
 
@@ -33,6 +37,10 @@ def get_episodes_info(season: int, url: str, headless: bool = True):
             if int(season_number) == season:
                 season_btn.click()
                 break
+        else:
+            browser.close()
+            message = f"Season '{season}' does not exist on the given url ({url})."
+            raise AttributeError(message)
 
         # get info from all episode from that season and the name of the series
         episode_list = browser.find_element(By.XPATH, "/html/body/main/div[3]/div/div[3]/div[3]").find_elements(By.CSS_SELECTOR, "div.item[data-episode-id]:not(.unreleased)")
@@ -61,14 +69,70 @@ def get_episodes_info(season: int, url: str, headless: bool = True):
             }
             
             season_dict["episodes"].append(episode_dict)
+            print(f"Got episode data for '{episode_number}. {episode_title}'.")
         
+    # get download links
+    for index, episode in enumerate(season_dict["episodes"]):
+        # make post request for the download data associated with the current episode 
+        print(f"Requesting download data for '{episode["episode-number"]}. {episode["title"]}'.")
+        url = r"https://vizertv.in/includes/ajax/publicFunctions.php"
+        payload = {'downloadData': 2, 'id': int(episode["id"])}
+
+        response = requests.post(url, data=payload)
+
+        # check if response status code is valid before proceeding
+        if response.status_code != 200:
+            message = f"Unexpected status code for '{episode["episode-number"]}. {episode["title"]}': {response.status_code}"
+            raise requests.RequestException(message)
+
+        # get dictionary containing the response
+        response_json = response.json()
+
+        # get redirect links and subtitle link from response
+        for key in response_json:
+            entry: dict = response_json[key]
+            if "sub" in entry.keys(): 
+                orig_audio_redirect = entry["redirector"]
+                subtitles = entry["sub"]
+
+            else:
+                dub_audio_redirect = entry["redirector"]
+        
+        # get original audio download link from redirector
+        print("Requesting original audio download link.")
+        response = requests.get(f"https://vizertv.in/{orig_audio_redirect}")
+        html = BeautifulSoup(response.content, 'html.parser')
+
+        orig_audio_download_link = re.search(r'window\.location\.href=\".*(mixdrop.+)\"', str(html))
+        if orig_audio_download_link:
+            orig_audio_download_link = f"https://{orig_audio_download_link.group(1)}?download"
+        
+        # get dubbed audio download link from redirector
+        print("Requesting dubbed audio download link.")
+        response = requests.get(f"https://vizertv.in/{dub_audio_redirect}")
+        html = BeautifulSoup(response.content, 'html.parser')
+
+        dub_audio_download_link = re.search(r'window\.location\.href=\".*(mixdrop.+)\"', str(html))
+        if dub_audio_download_link:
+            dub_audio_download_link = f"https://{dub_audio_download_link.group(1)}?download"
+        
+        # save data to season_dict
+        download_dict = {
+            "original-audio": orig_audio_download_link,
+            "dubbed-audio": dub_audio_download_link,
+            "subtitles": subtitles
+        }
+
+        season_dict["episodes"][index].update({"downloads": download_dict})
+        print(f"Got download data for '{episode["episode-number"]}. {episode["title"]}'.")
+  
     return season_dict
 
 
 if __name__ == "__main__":
     season = 2
     url = "https://vizertv.in/serie/online/doctor-who"
-    season_2 = get_episodes_info(season, url, headless=False)
+    season_2 = get_episodes_info(season, url, headless=True)
 
     with open("output/doctor-who-s02.json", 'w') as file:
         json.dump(season_2, file, indent=2)
